190445c536cac9da6caf4837463b6c52f930f15b
nmishrablog <31518505+nmishrablog@users.noreply.github.com>
2020-01-18 19:17:51 +0530

    Create config.js

---
+++ .vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	title: 'Wisdom Curated',
+	description: 'Human Curated, timely, consice and action oriented wisdom.',
+	ga: '', // 'UA-124245419-1',
+	themeConfig: {
+		
+		nav: [
+			 { text: 'Home', link: '/' },
+			{ text: 'Topics', link: '/topics/' }	
+		]
+	}
+}
